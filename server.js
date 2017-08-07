1f700174e3e28e8fe549df41c2d4aff9d71f5d59
sombek <sombek990@hotmail.com>
2017-08-07 09:09:45 +0300

    d

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
-// server requirments 
-  var express = require('express'); //express is framework on top of nodeJS
-  var app = express();  // initilize express
+  var express = require('express'); 
+  //express is framework on top of nodeJS
+  var app = express();  
+  // initilize express
 
   var session = require('express-session');
   var bodyParser = require('body-parser');
-  app.use(express.static(__dirname+'/public')); //to give the website the ability to visit public foldse
+
+  app.use(express.static(__dirname+'/public'));//to give the website the ability to visit public foldse
+
   var nodemailer = require('nodemailer'); // Nodemailer for sending emails for the users
 
   var transporter = nodemailer.createTransport({ 
     // Nodemailer initiliztion
     service: 'gmail',
- // secure:true for port 465, secure:false for port 587
-
     auth: { //Your Email info
       user: 'teamtwocloud@gmail.com', 
       pass: 'team2123456'
     }
   });
+
   app.use(session({
     secret: "tHiSiSasEcRetStr",
     resave: true,
@@ -36,27 +38,27 @@
   });
 
   app.post('/process',(req,res)=>{
+    
     console.log('email',req.body.email);
     let mailOptions = {
     from: '"Wesal 👻"', 
-      // sender address
-    to: req.body.email, // list of receivers
+    to: req.body.email, 
     subject: 'Hello ✔', // Subject line
-    text: 'Hello.. and welcome to Wesal'+
-' We are happy to join us, '+
-'Tell us that you are facing a problem.'+
-' We wish you a happy day.'+
-      'Wesal group'
-};
+    text: 'Hello.. \n' + 'Welcome to Wesal' +
+          ' We are happy to join us, ' +
+          'Tell us that you are facing a problem.'+
+          ' We wish you a happy day. \n\n' + 'Wesal group'
+    };
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
         return console.log(error);
     }
     console.log('Message %s sent: %s', info.messageId, info.response);
-});
-    res.send('done');
+    });
+    
+    res.render('layouts/main', { title: 'Done'});
     
   });
 
